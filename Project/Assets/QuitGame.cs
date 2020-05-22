731d238ed9d1e3d8ee53bb9e1e648a1822eb7655
mwmccoy <mwmccoy@andrew.cmu.edu>
2020-05-22 12:02:00 -0400

    Project without extras
    Main files, but all ZFBrowser files are missing.

---
+++ Project/Assets/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+//Very simple script to quit the game
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
