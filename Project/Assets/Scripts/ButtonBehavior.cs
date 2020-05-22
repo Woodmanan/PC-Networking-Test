da655a3e14c56836a9fa435c5fc0e6b04921cb05
jnamovic <jon.namovic@gmail.com>
2020-05-22 12:35:47 -0400

    added a cube that pulls up a new broswer window

---
+++ Project/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    private int count = 0;
+    public GameObject textbox;
+    private TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = textbox.GetComponent<TextMeshProUGUI>();
+        Debug.Log(text.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IsClicked() {
+        count++;
+        text.text = "I was clicked " + count + " times.";
+        Application.OpenURL("http://unity3d.com/");
+    }
+}
---
+++ Project/Assets/Scripts/CubePress.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePress : MonoBehaviour
+{
+    public string URL = "https://unity.com/";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // opens he given url in a new window
+        Application.OpenURL(URL);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
